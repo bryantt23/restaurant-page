daadfba77c30cea9c36d17e60c19513928cd9a22
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-26 00:32:37 -0800

    Move to another module

---
+++ pages/initialPage.js
@@ -0,0 +1,18 @@
+function initialPage() {
+  console.log('initialPage!');
+  const contentId = document.querySelector('#content');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Jack in the Box';
+  const img = document.createElement('img');
+  img.src = '/images/VegNews.JackInTheBox.PlantbasedUnChicken.jpg';
+
+  const h3 = document.createElement('h3');
+  h3.textContent = 'Vegetarian Chicken';
+
+  contentId.appendChild(h1);
+  contentId.appendChild(img);
+  contentId.appendChild(h3);
+}
+
+export default initialPage;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
+import initialPage from '../pages/initialPage';
+
 console.log('hello wor!');
 
-const contentId = document.querySelector('#content');
-
-const h1 = document.createElement('h1');
-h1.textContent = 'Jack in the Box';
-const img = document.createElement('img');
-img.src = '/images/VegNews.JackInTheBox.PlantbasedUnChicken.jpg';
-
-const h3 = document.createElement('h3');
-h3.textContent = 'Vegetarian Chicken';
-
-contentId.appendChild(h1);
-contentId.appendChild(img);
-contentId.appendChild(h3);
+initialPage();
