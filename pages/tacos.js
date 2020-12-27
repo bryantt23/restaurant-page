886b51b9864967057b2884ffbe080e428b33f052
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-26 19:14:52 -0800

    Make tabs clearer

---
+++ pages/tacos.js
@@ -0,0 +1,23 @@
+function loadTacos() {
+  const contentId = document.querySelector('#content');
+
+  if (contentId.hasChildNodes()) {
+    while (contentId.firstChild) {
+      contentId.removeChild(contentId.firstChild);
+    }
+  }
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Jack in the Box';
+  const img = document.createElement('img');
+  img.src = '/images/tacos.jpg';
+
+  const h3 = document.createElement('h3');
+  h3.textContent = 'Tacos';
+
+  contentId.appendChild(h1);
+  contentId.appendChild(img);
+  contentId.appendChild(h3);
+}
+
+export default loadTacos;
