c17441d3a034d3011b8ccfcc36ad63d6c1e70cfb
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-26 19:37:13 -0800

    Follow instructions from https://webpack.js.org/guides/asset-management/

--- pages/veggieChicken.js
+++ pages/veggieChicken.js
@@ -10,7 +10,7 @@ function loadVeggieChicken() {
   const h1 = document.createElement('h1');
   h1.textContent = 'Jack in the Box';
   const img = document.createElement('img');
-  img.src = '/dist/images/VegNews.JackInTheBox.PlantbasedUnChicken.jpg';
+  img.src = '/src/images/veggie_chicken.jpg';
 
   const h3 = document.createElement('h3');
   h3.textContent = 'Vegetarian Chicken';
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,22 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
   }
 };
